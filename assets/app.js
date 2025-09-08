03b9102a11dd1acbeb9702f07656b52517d8edb4
faizal <jai.crys@gmail.com>
2025-09-08 15:49:51 +0800

    Remove Tailwind CSS configuration file and clean up app.css by removing Preline variants import. Update Symfony configuration to comment out the Tailwind config file reference.

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,6 @@
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
-import 'preline/variants.css';
 import 'preline';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
