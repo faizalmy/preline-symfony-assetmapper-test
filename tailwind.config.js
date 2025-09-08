097405dfbecc519a90b9df8766b1bedfb3c78e78
faizal <jai.crys@gmail.com>
2025-09-08 13:51:24 +0800

    Add Tailwind CSS configuration file and update Symfony configuration to use it

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './assets/**/*.js',
+    './templates/**/*.html.twig',
+    './src/**/*.php',
+    './assets/vendor/preline/**/*.js',
+    './node_modules/preline/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
