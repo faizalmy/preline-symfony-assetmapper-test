cced11a481c59e3562db06711484a40a8aa2abaa
faizal <jai.crys@gmail.com>
2025-09-08 15:06:27 +0800

    Update app.js to import Preline variants CSS and clean up app.css by removing redundant import statement.

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,7 @@
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import 'preline/variants.css';
 import 'preline';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
